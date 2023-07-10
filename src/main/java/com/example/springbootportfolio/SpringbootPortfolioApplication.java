bc3b3459ba98c90103aaa95c1c5dfef6b56e06bd
KazueNaito <k-naito@KazueNaitonoMacBook-Pro.local>
2023-07-10 14:07:02 +0900

    プロジェクト作成

---
+++ src/main/java/com/example/springbootportfolio/SpringbootPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootportfolio/SpringbootPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
